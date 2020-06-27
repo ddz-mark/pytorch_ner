f284554c2f164baeaa98dc66f56a8a04afa3dcf8
“ddz-mark” <dudaizhong@126.com>
2020-06-27 17:24:02 +0800

    增加测试代码，base 准确率在95%

--- ModelHandler.py
+++ ModelHandler.py
@@ -20,17 +20,17 @@ class ModelHandler(nn.Module):
         self.device = params['device']
         self.word2id = params['word2id']
         self.tag2id = params['tag2id']
-        self.dnn = params['model'].to(self.device)
+        self.model = params['model'].to(self.device)
 
     def forword(self, features, lengths):
-        return self.dnn(features, lengths)
+        return self.model(features, lengths)
 
     #     def reset(self, m):
     #         if hasattr(m, 'reset_parameters'):
     #             torch.cuda.manual_seed(1)
     #             m.reset_parameters()
 
-    def fit(self, X_train, y_train, loss_fn, optimizer, modelPath=None, eval_set=None, early_stopping_rounds=None,
+    def fit(self, X_train, y_train, loss_fn, optimizer, model_path=None, eval_set=None, early_stopping_rounds=None,
             valBatchSize=None, verbose=0, temperature=1):
 
         if eval_set != None:
@@ -65,6 +65,8 @@ class ModelHandler(nn.Module):
                 y_train_var = y_train[t1 * batch_size:(t1 + 1) * batch_size]
                 X_train_var, lengths = tensorized(X_train_var, self.word2id)
                 y_train_var, _ = tensorized(y_train_var, self.tag2id)
+                X_train_var = X_train_var.to(self.device)
+                y_train_var = y_train_var.to(self.device)
 
                 self.zero_grad()
                 scores = self.forword(X_train_var, lengths)
@@ -88,8 +90,8 @@ class ModelHandler(nn.Module):
                 best_val_loss = val_loss
                 bestEpoch = epoch
                 count = 0
-                if modelPath != None:
-                    torch.save(self.state_dict(), modelPath)
+                if model_path != None:
+                    torch.save(self.state_dict(), model_path)
             elif early_stopping_rounds != None:
                 count += 1
                 if count >= early_stopping_rounds:
@@ -141,183 +143,34 @@ class ModelHandler(nn.Module):
         if isTrain == True:
             return valLoss / float(batchNum)
 
-    def predict_proba(self, testDF, inputType='tensor', temperature=1):
-        self.eval()
-        torch.set_grad_enabled(False)
-        if inputType == 'tensor':
-            scores = self.forword(testDF) / temperature
-            return torch.sigmoid(scores).squeeze()
-        elif inputType == 'DataFrame':
-            return torch.sigmoid(
-                self.forword(torch.tensor(np.array(testDF), dtype=torch.float32, device=self.device)) / temperature)
-        torch.set_grad_enabled(True)
+    def test(self, word_lists, tag_lists, indices):
+        """返回最佳模型在测试集上的预测结果"""
+        # 准备数据
+        # word_lists, tag_lists, indices = sort_by_lengths(word_lists, tag_lists)
+        tensorized_sents, lengths = tensorized(word_lists, self.word2id)
+        tensorized_sents = tensorized_sents.to(self.device)
 
-    # 改进地方
-    def predict(self, testDF, inputType='tensor', temperature=1, threshold=0.5):
-        predict_proba = self.predict_proba(testDF, inputType, temperature).cpu().numpy().tolist()
-        return predict_proba
+        self.model.eval()
+        with torch.no_grad():
+            batch_tagids = self.model.test(
+                tensorized_sents, lengths, self.tag2id)
 
-    def getAUC(self, y_true, y_score):
-        return roc_auc_score(y_true.detach().cpu().numpy(),
-                             y_score.detach().cpu().numpy())
+        # 将id转化为标注
+        pred_tag_lists = []
+        id2tag = dict((id_, tag) for tag, id_ in self.tag2id.items())
+        for i, ids in enumerate(batch_tagids):
+            tag_list = []
+            for j in range(lengths[i] - 1):  # crf解码过程中，end被舍弃
+                tag_list.append(id2tag[ids[j].item()])
+            pred_tag_lists.append(tag_list)
 
-# class ModelHandler(nn.Module):
-#     def __init__(self, params):
-#         super(ModelHandler, self).__init__()
-#         self.device = params['device']
-#         self.epoch_num = params['epoch_num']
-#         self.train_batch_size = params['train_batch_size']
-#         self.val_batch_size = params['val_batch_size']
-#         self.model = params['model'].to(self.device)
-#
-#     def forward(self, x):
-#         return self.model(x)
-#
-#     def fit(self, train_iter, loss_fn, optimizer, modelPath=None,
-#             val_iter=None, early_stopping_rounds=None, verbose=2):
-#         '''
-#         early_stopping_rounds：是否使用 early_stop 来防止过拟合
-#         verbose：2分类还是多分类
-#         '''
-#
-#         if early_stopping_rounds != None:
-#             # 用来计数多少epoch在验证集上的结果没有改进了
-#             count = 0
-#
-#         batch_size = self.train_batch_size
-#         batchNumInEveryEpoch = len(train_iter)
-#
-#         epochNums = self.epoch_num
-#         best_val_acc = -1000000
-#         best_val_loss = 1000000
-#
-#         num = 0
-#         for epoch in range(epochNums):
-#             print('************************* epoch:', epoch, '*************************')
-#
-#             self.train()
-#             torch.set_grad_enabled(True)
-#
-#             # 随机化数据
-#             #             randPermNums = np.random.permutation(trainDataNum)
-#             #             X_train = X_train[randPermNums]
-#             #             y_train = np.array(y_train)[randPermNums]
-#
-#             trainAcc = 0.0
-#             trainLoss = 0.0
-#             l2Loss = 0.0
-#             for index, (X_train_var, y_train_var) in enumerate(train_iter):
-#                 X_train_var = (i.to(self.device) for i in X_train_var)
-#
-#                 # 将 label 转为 one-hot编码，这里针对多分类和二分类的softmax形式。如果是二分类的sogmid，则注释
-#                 # y_train_var = y_train_var.unsqueeze(1)
-#                 # y_train_var = torch.zeros(len(y_train_var), verbose).scatter_(1, y_train_var, 1)
-#                 y_train_var = y_train_var.to(self.device)
-#
-#                 self.zero_grad()
-#                 #                 print (X_train_var.shape)
-#                 scores = self.forward(X_train_var)
-#                 loss = loss_fn(scores.squeeze(), y_train_var)
-#
-#                 trainAcc = trainAcc + self.getAUC(y_train_var, torch.sigmoid(scores).squeeze())
-#                 trainLoss = trainLoss + loss.data.item()
-#                 # l2Loss = l2Loss + regularization_loss(self.model)
-#
-#                 self.train()
-#                 torch.set_grad_enabled(True)
-#                 loss.backward()
-#                 optimizer.step()
-#             if verbose == 2:
-#                 print('train auc:', trainAcc / float(batchNumInEveryEpoch))
-#                 print('train loss:', trainLoss / float(batchNumInEveryEpoch))
-#                 # print('train l2_loss:', l2Loss / float(batchNumInEveryEpoch))
-#
-#             val_acc, val_loss = self.check_accuracy(self, val_iter, self.val_batch_size, loss_fn,
-#                                                     True, verbose)
-#             if verbose == 2:
-#                 print('val_auc:', val_acc)
-#                 print('val_loss:', val_loss)
-#             #     print ('val_acc:', val_acc, file=file, flush=True)
-#             if val_acc > best_val_acc:
-#                 #             if val_loss < best_val_loss:
-#                 best_val_acc = val_acc
-#                 best_val_loss = val_loss
-#                 bestEpoch = epoch
-#                 count = 0
-#                 if modelPath != None:
-#                     torch.save(self.state_dict(), modelPath)
-#             elif early_stopping_rounds != None:
-#                 count += 1
-#                 if count >= early_stopping_rounds:
-#                     if verbose >= 1:
-#                         print('Stopping.')
-#                         print('Best Epoch:', bestEpoch)
-#                         print('Best Val Auc:', best_val_acc)
-#                         print('Best Val Loss:', best_val_loss)
-#                     break
-#
-#     def check_accuracy(self, model, val_loader, valBatchSize, loss_fn,
-#                        isTrain, verbose, temperature=1):
-#         if verbose == 2:
-#             if isTrain:
-#                 print('*****Checking accuracy on validation set*****')
-#             #         print('Checking accuracy on validation set', file=file, flush=True)
-#             else:
-#                 print('Checking accuracy on test set')
-#                 #         print('Checking accuracy on test set', file=file, flush=True)
-#         self.eval()
-#         torch.set_grad_enabled(False)
-#
-#         batchNum = len(val_loader)
-#         #         batchNum = X_val.shape[0] // valBatchSize
-#         #         if isTrain != True and X_val.shape[0] % valBatchSize != 0:
-#         #             batchNum += 1
-#         if verbose == 2:
-#             print('batchNum:', batchNum)
-#
-#         valAcc = 0.0
-#         valLoss = 0.0
-#         for index, (tX_val_var, tY_val_var) in enumerate(val_loader):
-#             tX_val_var = (i.to(self.device) for i in tX_val_var)
-#
-#             # tY_val_var = tY_val_var.unsqueeze(1)
-#             # tY_val_var = torch.zeros(len(tY_val_var), verbose).scatter_(1, tY_val_var, 1)
-#             tY_val_var = tY_val_var.to(self.device)
-#
-#             scores = self.forward(tX_val_var)
-#
-#             if isTrain == True:
-#                 loss = loss_fn(scores.squeeze(), tY_val_var / temperature)
-#                 valAcc += self.getAUC(tY_val_var / temperature, torch.sigmoid(scores).squeeze())
-#                 valLoss = valLoss + loss.data.item()
-#
-#         if isTrain == True:
-#             return valAcc / float(batchNum), valLoss / float(batchNum)
-#
-#     def predict_proba(self, testDF, inputType='tensor', temperature=1):
-#         self.eval()
-#         torch.set_grad_enabled(False)
-#         if inputType == 'tensor':
-#             testDF = testDF.reshape(testDF.shape[0], 1, -1).to(self.device)
-#             scores = self.forward(testDF) / temperature
-#             return torch.sigmoid(scores).squeeze()
-#         elif inputType == 'DataFrame':
-#             testDF = testDF.reshape(testDF.shape[0], 1, -1).to(self.device)
-#             return torch.sigmoid(self.forward(
-#                 torch.tensor(np.array(testDF), dtype=torch.float32, device=self.device)) / temperature).squeeze()
-#         torch.set_grad_enabled(True)
-#
-#     def predict(self, testDF, inputType='tensor', threshold=0.5, temperature=1):
-#         predict_proba = self.predict_proba(testDF, inputType, temperature).cpu().numpy().tolist()
-#         #         predict_lables = [1 if x >= threshold else 0 for x in predict_proba]
-#         return predict_proba
-#
-#     def getAUC(self, y_true, y_score):
-#         #         y_true = y_true.detach().cpu().numpy()
-#         #         y_score = y_score.detach().cpu().numpy()
-#         accracy = np.mean((torch.argmax(y_score, 1) == y_true).numpy())
-#         return accracy
-#         # a, _, _ = np.unique(y_true.view(y_true.dtype.descr * y_true.shape[1]), return_index=True, return_inverse=True)
-#         # if len(a) == 1: # bug in roc_auc_score
-#         #   return accuracy_score(y_true, y_score)
-# #         return roc_auc_score(y_true, y_score)
+        # indices存有根据长度排序后的索引映射的信息
+        # 比如若indices = [1, 2, 0] 则说明原先索引为1的元素映射到的新的索引是0，
+        # 索引为2的元素映射到新的索引是1...
+        # 下面根据indices将pred_tag_lists和tag_lists转化为原来的顺序
+        ind_maps = sorted(list(enumerate(indices)), key=lambda e: e[1])
+        indices, _ = list(zip(*ind_maps))
+        pred_tag_lists = [pred_tag_lists[i] for i in indices]
+        tag_lists = [tag_lists[i] for i in indices]
+
+        return pred_tag_lists, tag_lists
